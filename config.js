395f4b5843217d307427ad019848bcaabc57c622
xandemelo84-glitch <xandemelo_84@hotmail.com>
2026-08-28 14:46:39 -0300

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,7 @@
 // Cole SOMENTE a Publishable key do Supabase.
 // Nunca coloque aqui uma secret/service_role key.
 
-window.MCW_CONFIG = {
-  url: "https://vvlsqfwsmvtbxkexiarn.supabase.co",
-sb_publishable_lXmEwI6CmArOIELQRnNVig_S-8UJwef};
+janela.MCW_CONFIG = {
+    URL:"https://vylsqfwsmvtbxkexiarn.supabase.co",
+    chave:"SUA_CHAVE_PUBLICÁVEL_AQUI"
+};
